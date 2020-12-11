49a57ff555e3846e814012b30c66e1f4fba1708f
Kangketik <info@kangketik.web.id>
2020-12-11 14:48:39 +0700

    Initial Commit

---
+++ samples/samplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  samplesApp.swift
+//  samples
+//
+//  Created by Gilang Pratama Priadi on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct samplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
